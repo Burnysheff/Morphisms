c54ecb9f6d5dec1ab37af8e82f943e9a65336b74
Nikolai Burnyshev <burnyshevni@gmail.com>
2022-05-13 14:58:00 +0300

    JARs

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.morphims {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.xml;
-
-
-    opens com.example.morphims to javafx.fxml;
-    exports com.example.morphims;
-}
